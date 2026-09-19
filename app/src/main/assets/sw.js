0cc1e09c4a6e9757d398c64cc144f4753067b1f2
dossgroups8-sys <dossgroups8@gmail.com>
2026-09-19 15:59:56 +0000

    Create sw.js

---
+++ app/src/main/assets/sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'badegnan-os-shell-v1';
+const CORE = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(cache => cache.put(req, copy));
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
